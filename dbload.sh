5c22c691254d8a68702c8f5f460242d5bb290dba
Zain Ali <muhammad.jafarkhan@mail.mcgill.ca>
2022-05-27 21:29:43 -0400

    loading the dvdrental db script

---
+++ dbload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This script will be used to load the dvdrental database
+
+#Get the script from the github repo
+REPO='https://github.com/ZainAli1995/PostgreSQL_Configs/blob/main/dvdrental.zip'
+
+wget ${REPO}
